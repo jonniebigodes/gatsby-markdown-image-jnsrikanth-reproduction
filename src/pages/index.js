56ed154176a2a30293dae1359b77856d755761df
jonniebigodes <jonniebigodes@hotmail.com>
2019-08-15 21:57:24 +0100

    chore: update index link markdown and no console

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+export default () => (
+  <div>
+    Markdown items
+    <br />
+    <ul style={{
+        listStyleType:'none'
+    }}>
+      <li>
+        <Link to={"/services/accounting/"}>
+          Firs markdown page, services/accounting
+        </Link>
+      </li>
+      <li>
+        <Link to={"/services/randomservice/"}>
+          second markdown page, services/randomservice
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
--- src/templates/dummy-template.js
+++ src/templates/dummy-template.js
@@ -2,8 +2,6 @@ import React from 'react'
 import {graphql } from 'gatsby'
 import Img from 'gatsby-image'
 const DummyTemplate=({ data })=>{
-    console.log(data);
-    
     return (
         <div>
             <h1>
